81886d7f7411b55bea0a5786ecfe926d9d80caef
Lawwee <lawalmohammed567@gmail.com>
2023-07-27 18:24:39 +0100

    added getAllGoals function and createGoal function

--- controllers/goal.controller.js
+++ controllers/goal.controller.js
@@ -4,3 +4,23 @@ import { BadRequestError, UnauthenticatedError } from '../errors';
 import Goal from '../models/goal.model';
 import logger from '../utils/winston';
 
+// Get all goals controller
+export const getAllGoals = asyncHandler(async (req, res) => {
+    const goal = await Goal.find({ user: req.user._id });
+
+    res.status(StatusCodes.OK).json(goal);
+});
+
+// Set a goal controller
+export const createGoal = asyncHandler(async (req, res) => {
+    const { goalAmount, targetDate, description } = req.body;
+    const goal = new Goal ({
+        user: req.user._id,
+        goalAmount: goalAmount,
+        targetDate: targetDate,
+        description: description,
+        createdOn: new Date
+    });
+
+    res.status(StatusCodes.OK).json(goal);
+});
